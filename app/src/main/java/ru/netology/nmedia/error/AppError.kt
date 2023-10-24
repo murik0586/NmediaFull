4757923e26b69b30a0ee88ed2a6c82f9c662e567
murik0586 <murikmusaev86@gmail.com>
2023-10-24 03:20:05 +0300

    Andad-38  homework 4: LoadAdapter

--- app/src/main/java/ru/netology/nmedia/error/AppError.kt
+++
@@ -1,6 +0,0 @@
-package ru.netology.nmedia.error
-
-sealed class AppError(var code: String): RuntimeException()
-class ApiError(val status: Int, code: String): AppError(code)
-object NetworkError : AppError("error_network")
-object UnknownError: AppError("error_unknown")
